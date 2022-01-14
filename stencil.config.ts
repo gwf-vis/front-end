8b83b9f0d7a10462ed3e19cd9f3bfedda045a1bf
Sean Wong <seanwong24@live.com>
2022-01-14 19:15:41 +0000

    update env config

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,14 @@ import { Config } from '@stencil/core';
 
 // https://stenciljs.com/docs/config
 
+const devEnv = {
+  SERVER_BASE_URL: 'http://localhost:5000',
+};
+
+const prodEnv = {
+  SERVER_BASE_URL: 'backend',
+};
+
 export const config: Config = {
   globalScript: 'src/global/app.ts',
   globalStyle: 'src/global/app.css',
@@ -12,7 +20,5 @@ export const config: Config = {
       serviceWorker: null,
     },
   ],
-  env: {
-    SERVER_BASE_URL: 'http://localhost:5000',
-  },
+  env: process.env.NODE_ENV === 'dev' ? devEnv : prodEnv,
 };
